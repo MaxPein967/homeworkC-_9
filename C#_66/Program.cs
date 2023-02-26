6f532142db4ca91205b0f37ac83409fb8fdd3fb7
Sergei Vasiliev <9670105747@mail.ru>
2023-02-26 12:54:36 +0700

    Task 66

--- C#_66/Program.cs
+++ C#_66/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+int m = ReadInt("Введите m: ");
+int n = ReadInt("Введите n: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSum(m, n, temp=0);
+
+void PrintSum(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"-> {summ} ");
+    return;
+  }
+  PrintSum(m, n - 1, summ);
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
